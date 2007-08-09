56e9a3d8cee07ec8161922e03b6210c0d5f643a7
Jerome Quelin <jquelin@cpan.org>
2007-08-09 09:13:14 +0000

    Import of JQUELIN/Acme-MetaSyntactic-buzzwords-0.01 from CPAN.
    gitpan-cpan-distribution: Acme-MetaSyntactic-buzzwords
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         JQUELIN/Acme-MetaSyntactic-buzzwords-0.01.tar.gz
    gitpan-cpan-author:       JQUELIN
    gitpan-cpan-maturity:     released

---
+++ lib/Acme/MetaSyntactic/buzzwords.pm
@@ -0,0 +1,34 @@
+#
+# This file is part of Acme::MetaSyntactic::buzzwords.
+# Copyright (c) 2007 Jerome Quelin, all rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the same terms as Perl itself.
+#
+#
+
+package Acme::MetaSyntactic::buzzwords;
+
+use strict;
+use warnings;
+
+use base qw[ Acme::MetaSyntactic::List ];
+
+our $VERSION = '0.01';
+__PACKAGE__->init();
+
+=head1 NAME
+
+Acme::MetaSyntactic::buzzwords - Pointy-haired boss lingo
+
+
+=cut
+
+
+1;
+
+__DATA__
+# names
+jq
+test 
+
